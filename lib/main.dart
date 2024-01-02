5c01f2149d0b6c034940a06dd71ee17b298afd66
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-02 19:55:47 +0500

    optional position parameter test

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -31,8 +30,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void main() {
+  String joinWithCommas(int a, [int? b, int? c, int? d, int? e]) {
+    return TODO();
+  }
 
+// Tests your solution (Don't edit!):
+  void main() {
+    final errs = <String>[];
+
+    try {
+      final value = joinWithCommas(1);
+
+      if (value != '1') {
+        errs.add(
+            'Tried calling joinWithCommas(1) \n and got $value instead of the expected (\'1\').');
+      }
+    } on UnimplementedError {
+      print(
+          'Tried to call joinWithCommas but failed. \n Did you implement the method?');
+      return;
+    } catch (e) {
+      print(
+          'Tried calling joinWithCommas(1), \n but encountered an exception: ${e.runtimeType}.');
+      return;
+    }
+
+    try {
+      final value = joinWithCommas(1, 2, 3);
+
+      if (value != '1,2,3') {
+        errs.add(
+            'Tried calling joinWithCommas(1, 2, 3) \n and got $value instead of the expected (\'1,2,3\').');
+      }
+    } on UnimplementedError {
+      print(
+          'Tried to call joinWithCommas but failed. \n Did you implement the method?');
+      return;
+    } catch (e) {
+      print(
+          'Tried calling joinWithCommas(1, 2 ,3), \n but encountered an exception: ${e.runtimeType}.');
+      return;
+    }
+
+    try {
+      final value = joinWithCommas(1, 2, 3, 4, 5);
+
+      if (value != '1,2,3,4,5') {
+        errs.add(
+            'Tried calling joinWithCommas(1, 2, 3, 4, 5) \n and got $value instead of the expected (\'1,2,3,4,5\').');
+      }
+    } on UnimplementedError {
+      print(
+          'Tried to call joinWithCommas but failed. \n Did you implement the method?');
+      return;
+    } catch (e) {
+      print(
+          'Tried calling stringify(1, 2, 3, 4 ,5), \n but encountered an exception: ${e.runtimeType}.');
+      return;
+    }
+
+    if (errs.isEmpty) {
+      print('Success!');
+    } else {
+      errs.forEach(print);
+    }
   }
 
   @override
