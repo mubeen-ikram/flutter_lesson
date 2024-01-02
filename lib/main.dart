3669303c427178fd69b5f30585bdb985e44747c5
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-02 19:31:00 +0500

    Cascades test

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,72 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  void main() {
+class BigObject {
+  int anInt = 0;
+  String aString = '';
+  List<double> aList = [];
+  bool _done = false;
 
+  void allDone() {
+    _done = true;
+  }
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  BigObject fillBigObject(BigObject obj) {
+    // Create a single statement that will update and return obj:
+    return TODO('obj..');
+  }
+
+// Tests your solution (Don't edit!):
+  void main() {
+    BigObject obj;
+
+    try {
+      obj = fillBigObject(BigObject());
+    } catch (e) {
+      print(
+          'Caught an exception of type ${e.runtimeType} \n while running fillBigObject');
+      return;
+    }
+
+    final errs = <String>[];
+
+    if (obj.anInt != 1) {
+      errs.add(
+          'The value of anInt was ${obj.anInt} \n rather than the expected (1).');
+    }
+
+    if (obj.aString != 'String!') {
+      errs.add(
+          'The value of aString was \'${obj.aString}\' \n rather than the expected (\'String!\').');
+    }
+
+    if (obj.aList.length != 1) {
+      errs.add(
+          'The length of aList was ${obj.aList.length} \n rather than the expected value (1).');
+    } else {
+      if (obj.aList[0] != 3.0) {
+        errs.add(
+            'The value found in aList was ${obj.aList[0]} \n rather than the expected (3.0).');
+      }
+    }
+
+    if (!obj._done) {
+      errs.add('It looks like allDone() wasn\'t called.');
+    }
+
+    if (errs.isEmpty) {
+      print('Success!');
+    } else {
+      errs.forEach(print);
+    }
   }
 
   @override
