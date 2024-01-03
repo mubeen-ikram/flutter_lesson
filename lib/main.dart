e86ac2a34550ed70dbc626cab3345d5032b3022b
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-03 14:54:27 +0500

    redirecting constructor test

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,56 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  void main() {
+class Color {
+  int red;
+  int green;
+  int blue;
 
+  Color(this.red, this.green, this.blue);
+
+// TODO: Create a named constructor called "black" here
+// and redirect it to call the existing constructor
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+// Tests your solution (Don't edit!):
+  void main() {
+    final errs = <String>[];
+
+    try {
+      final result = Color.black();
+
+      if (result.red != 0) {
+        errs.add(
+            'Called Color.black() and got a Color with red equal to \n ${result.red} instead of the expected value (0).');
+      }
+
+      if (result.green != 0) {
+        errs.add(
+            'Called Color.black() and got a Color with green equal to \n ${result.green} instead of the expected value (0).');
+      }
+
+      if (result.blue != 0) {
+        errs.add(
+            'Called Color.black() and got a Color with blue equal to \n ${result.blue} instead of the expected value (0).');
+      }
+    } catch (e) {
+      print(
+          'Called Color.black() and got an exception of type ${e.runtimeType}.');
+      return;
+    }
+
+    if (errs.isEmpty) {
+      print('Success!');
+    } else {
+      errs.forEach(print);
+    }
   }
 
   @override
