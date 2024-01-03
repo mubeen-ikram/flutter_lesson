8d51c8c639aee4e25d80b69ec457ed78dbe21fc6
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-03 14:58:24 +0500

    const constructor test

--- lib/main.dart
+++ lib/main.dart
@@ -23,16 +23,52 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  void main() {
+class Recipe {
+  List<String> ingredients;
+  int calories;
+  double milligramsOfSodium;
 
+// TODO: Create a const constructor here"
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+// Tests your solution (Don't edit!):
+  void main() {
+    final errs = <String>[];
+
+    try {
+      const obj = Recipe(['1 egg', 'Pat of butter', 'Pinch salt'], 120, 200);
+
+      if (obj.ingredients.length != 3) {
+        errs.add(
+            'Called Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and got an object with ingredient list of length ${obj.ingredients.length} rather than the expected length (3).');
+      }
+
+      if (obj.calories != 120) {
+        errs.add(
+            'Called Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and got an object with a calorie value of ${obj.calories} rather than the expected value (120).');
+      }
+
+      if (obj.milligramsOfSodium != 200) {
+        errs.add(
+            'Called Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and got an object with a milligramsOfSodium value of ${obj.milligramsOfSodium} rather than the expected value (200).');
+      }
+    } catch (e) {
+      print(
+          'Tried calling Recipe([\'1 egg\', \'Pat of butter\', \'Pinch salt\'], 120, 200) \n and received a null.');
+    }
+
+    if (errs.isEmpty) {
+      print('Success!');
+    } else {
+      errs.forEach(print);
+    }
   }
 
   @override
