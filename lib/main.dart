acf3c72b42da6c5654d37b684c2de0081d417ee3
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-03 14:28:04 +0500

    list initializer test

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,53 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+class FirstTwoLetters {
+  final String letterOne;
+  final String letterTwo;
+
+  // TODO: Create a constructor with an initializer list here:
+  FirstTwoLetters(String word)
+
+}
 
 class _MyHomePageState extends State<MyHomePage> {
-  void main() {
 
+
+// Tests your solution (Don't edit!):
+  void main() {
+  final errs = <String>[];
+
+  try {
+  final result = FirstTwoLetters('My String');
+
+  if (result.letterOne != 'M') {
+  errs.add('Called FirstTwoLetters(\'My String\') and got an object with \n letterOne equal to \'${result.letterOne}\' instead of the expected value (\'M\').');
+  }
+
+  if (result.letterTwo != 'y') {
+  errs.add('Called FirstTwoLetters(\'My String\') and got an object with \n letterTwo equal to \'${result.letterTwo}\' instead of the expected value (\'y\').');
+  }
+  } catch (e) {
+  errs.add('Called FirstTwoLetters(\'My String\') and got an exception \n of type ${e.runtimeType}.');
+  }
+
+  bool caughtException = false;
+
+  try {
+  FirstTwoLetters('');
+  } catch (e) {
+  caughtException = true;
+  }
+
+  if (!caughtException) {
+  errs.add('Called FirstTwoLetters(\'\') and didn\'t get an exception \n from the failed assertion.');
+  }
+
+  if (errs.isEmpty) {
+  print('Success!');
+  } else {
+  errs.forEach(print);
+  }
   }
 
   @override
