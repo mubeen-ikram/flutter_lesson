b3e948f2db903346cc34ec124ba61a4b3aba0df1
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-03 14:19:47 +0500

    This in constructor test

--- lib/main.dart
+++ lib/main.dart
@@ -29,12 +29,45 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
+class MyClass {
+  final int anInt;
+  final String aString;
+  final double aDouble;
+
+// TODO: Create the constructor here.
+}
 
 class _MyHomePageState extends State<MyHomePage> {
-  void main() {
 
+
+// Tests your solution (Don't edit!):
+  void main() {
+  final errs = <String>[];
+
+  try {
+  final obj = MyClass(1, 'two', 3);
+
+  if (obj.anInt != 1) {
+  errs.add('Called MyClass(1, \'two\', 3) and got an object with anInt of ${obj.anInt} \n instead of the expected value (1).');
   }
 
+  if (obj.anInt != 1) {
+  errs.add('Called MyClass(1, \'two\', 3) and got an object with aString of \'${obj.aString}\' \n instead of the expected value (\'two\').');
+  }
+
+  if (obj.anInt != 1) {
+  errs.add('Called MyClass(1, \'two\', 3) and got an object with aDouble of ${obj.aDouble} \n instead of the expected value (3).');
+  }
+  } catch (e) {
+  print('Called MyClass(1, \'two\', 3) and got an exception \n of type ${e.runtimeType}.');
+  }
+
+  if (errs.isEmpty) {
+  print('Success!');
+  } else {
+  errs.forEach(print);
+  }
+  }
   @override
   Widget build(BuildContext context) {
     main();
