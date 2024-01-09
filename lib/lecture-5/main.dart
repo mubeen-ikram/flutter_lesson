ada0988cb0b9872996f365512b323ecf5b6d2a99
Mubeen <mubeen.ikram1999@gmail.com>
2024-01-09 18:41:53 +0500

    Favourite Functionality Added

--- lib/lecture-5/main.dart
+++ lib/lecture-5/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         ),
         home: MyHomePage(),
       ),
@@ -23,19 +23,85 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  var favourites = <WordPair>[];
+
+  void nextPair() {
+    current = WordPair.random();
+    notifyListeners();
+  }
+
+  void toggleFavourite() {
+    if (checkFavourite()) {
+      favourites.remove(current);
+    } else {
+      favourites.add(current);
+    }
+    notifyListeners();
+  }
+
+  bool checkFavourite() {
+    return favourites.contains(current);
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
-
+    var currentText = appState.current;
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(currentText: currentText),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: () => appState.toggleFavourite(),
+                    child: Row(
+                      children: [
+                        Icon(appState.checkFavourite()
+                            ? Icons.favorite
+                            : Icons.favorite_outline),
+                        Text("Like"),
+                      ],
+                    )),
+                SizedBox(
+                  width: 10,
+                ),
+                ElevatedButton(
+                    onPressed: () => appState.nextPair(), child: Text("Next")),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.currentText,
+  });
+
+  final WordPair currentText;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(currentText.asLowerCase, style: style),
       ),
     );
   }
